b6cf7c094e87290b4a5b04704aa887ebc7e46779
srbova <srbova.ceca@gmail.com>
2018-12-21 19:09:12 +0100

    Task: created database structure

---
+++ database/migrations/2018_12_21_180125_create_amas_table.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAmasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('amas', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title',255);
+            $table->text('content');
+            $table->string('person',50);
+            $table->string('tags',255);
+            
+            $table->timestamps();
+        });
+
+
+        Schema::table('amas', function($table) {
+
+            $table->unsignedInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+
+        });
+
+        Schema::table('amas', function($table) {
+            
+            $table->unsignedInteger('ama_announcement_id');
+            $table->foreign('ama_announcement_id')->references('id')->on('ama_announcements');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('amas');
+    }
+}
